2d5b9ef269e6d8f18fc761e8986706b036760f4b
Rudenko Mykhailo <fernandosolotop@gmail.com>
2025-12-17 21:39:26 +0200

    feat(background-component): add animated character image

---
+++ src/components/BackgroundCharacter/BackgroundCharacter.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+import { computed, watch, ref } from 'vue'
+const router = useRoute()
+const currentPage = computed(() => router.name)
+const isCharactersPage = ref(true)
+
+watch(currentPage, () => {
+  isCharactersPage.value = currentPage.value === 'characters'
+})
+</script>
+
+<template>
+  <Transition name="fade">
+    <img
+      v-show="!isCharactersPage"
+      class="bg-image-character"
+      src="/src/media/images/ichigo-bankai-full.png"
+  /></Transition>
+</template>
+
+<style scoped>
+.bg-image-character {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  width: 60vw;
+  /* opacity: 0.6; */
+  filter: brightness(0.8);
+  height: 80%;
+  z-index: -9;
+  object-fit: cover;
+  pointer-events: none;
+  user-select: none;
+
+  -webkit-mask-image: radial-gradient(ellipse at center, #000 15%, #000 50%, transparent 70%);
+  mask-image: radial-gradient(ellipse at center, #000 15%, #000 50%, transparent 70%);
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease-in-out;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ src/components/BackgroundComponent/BackgroundComponent.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import BackgroundCharacter from '../BackgroundCharacter/BackgroundCharacter.vue'
+</script>
+
+<template>
+  <div class="background">
+    <video
+      class="bg-video"
+      autoplay
+      loop
+      muted
+      playsinline
+      preload="auto"
+      type="video/mp4"
+      webkit-playsinline
+      poster="/src/media/images/bg-static.png"
+      src="/src/media/video/bg-video.mp4"
+    ></video>
+    <div class="vignette"></div>
+    <BackgroundCharacter />
+    <img class="mask" src="/src/media/images/mask-black.png" />
+  </div>
+</template>
+
+<style scoped>
+.background {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -10;
+
+  .vignette {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    background: radial-gradient(circle at center, transparent 30%, rgba(0, 0, 0, 0.8) 100%);
+  }
+  .bg-video {
+    user-select: none;
+    pointer-events: none;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    z-index: -10;
+  }
+  .mask {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    z-index: -8;
+    object-fit: cover;
+    pointer-events: none;
+    user-select: none;
+  }
+}
+</style>
