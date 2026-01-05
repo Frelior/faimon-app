8ff626e61fd9ddfcb934eec4072556212108b57d
Rudenko Mykhailo <fernandosolotop@gmail.com>
2026-01-05 21:29:37 +0200

    build 3

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import { supabase } from '@/lib/supabaseClient'
 
 const router = createRouter({
-  history: createWebHashHistory('/faimon-app/'),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/:pathMatch(.*)*',
