323c09fe87a63298aa525749368b2fe19a2ad78b
Rudenko Mykhailo <fernandosolotop@gmail.com>
2026-01-07 23:04:31 +0200

    clear branch

--- README.md
+++
@@ -1,1 +0,0 @@
-faimon bleach website
--- src/services/storage.ts
+++
@@ -1,7 +0,0 @@
-export function isFirstLoading(): boolean {
-  return !localStorage.getItem('loadedBefore')
-}
-
-export function markAsLoaded() {
-  localStorage.setItem('loadedBefore', 'true')
-}
